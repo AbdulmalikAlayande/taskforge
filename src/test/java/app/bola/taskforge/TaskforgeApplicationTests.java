87127419c3e077503619a0637cd29f3539623076
AbdulmalikAlayande <alaabdulmalik03@gmail.com>
2025-06-02 20:36:40 +0100

    Initial Commit

---
+++ src/test/java/app/bola/taskforge/TaskforgeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.bola.taskforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskforgeApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
