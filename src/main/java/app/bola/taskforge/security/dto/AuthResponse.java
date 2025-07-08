d21223a7433eae5c7b3db17c60acf6b3ade0ad21
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2025-07-08 11:34:32 +0100

    Implementing Security

--- src/main/java/app/bola/taskforge/security/dto/AuthResponse.java
+++ src/main/java/app/bola/taskforge/security/dto/AuthResponse.java
@@ -3,7 +3,6 @@ package app.bola.taskforge.security.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.Value;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
