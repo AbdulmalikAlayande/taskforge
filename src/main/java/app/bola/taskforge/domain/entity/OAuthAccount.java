be843d12bcafe7d235a952b420dd15a2c7e1f38a
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2025-10-04 12:27:41 +0100

    Add Lombok Getter and Setter to OAuthAccount entity

--- src/main/java/app/bola/taskforge/domain/entity/OAuthAccount.java
+++ src/main/java/app/bola/taskforge/domain/entity/OAuthAccount.java
@@ -4,11 +4,15 @@ import app.bola.taskforge.common.entity.BaseEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.experimental.SuperBuilder;
 
 
 @Entity
+@Getter
+@Setter
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
