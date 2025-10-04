ce938257261f35ae2eb1e3f52159efc6af6a204a
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2025-10-04 12:22:35 +0100

    Add OAuthAccountRepository interface for OAuth account persistence

---
+++ src/main/java/app/bola/taskforge/repository/OAuthAccountRepository.java
@@ -0,0 +1,4 @@
+package app.bola.taskforge.repository;
+
+public interface OAuthAccountRepository extends org.springframework.data.repository.Repository<app.bola.taskforge.domain.entity.OAuthAccount, java.lang.String> {
+}
