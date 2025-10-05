8e26fa0be21373b078d18851745f5aa7239b9e2a
AbdulmalikAlayande <114596864+AbdulmalikAlayande@users.noreply.github.com>
2025-10-05 22:35:39 +0100

    Update INVITATION_URL format to use dynamic frontend URL

--- src/main/java/app/bola/taskforge/common/utilities/Constants.java
+++ src/main/java/app/bola/taskforge/common/utilities/Constants.java
@@ -1,7 +1,8 @@
 package app.bola.taskforge.common.utilities;    
 
 public class Constants {
+	
     public static final String FRONTEND_URL = "http://localhost:3000";
     public static final String INVITATION_SUBJECT = "Invitation Acceptance";
-    public static final String INVITATION_URL = "http://localhost:3000/onboarding/member/accept-invite/%s/%s"; // token, organizationId
+    public static final String INVITATION_URL = "%s/onboarding/member/accept-invite/%s/%s"; // token, organizationId
 }
